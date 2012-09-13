fcc176fcbe8f88ea1d20b00269cd33bc4e5778b6
Jim Zhang <Hyriodula@gmail.com>
2012-09-13 18:12:18 -0400

    fixed town portal scrolls (stupid error)

--- item_actions.py
+++ item_actions.py
@@ -2,19 +2,17 @@ import display
 # Use effects items can have.
 
 def tp_target_to(level, x, y):
-	def x(target):
+	def f(target):
 		display.msg("You start to channel the spell...")
 		target.cur_level.advance_ticks(1000)
 		if target in target.cur_level.creatures:
 			display.msg("Your surroundings suddenly change.")
-			target.change_level_to(level)
-			target.x = 12
-			target.y = 12
+			target.change_level_to(level, x, y)
 			return True
 		else:
 			# stupid player died while channeling
 			return False
-	return x
+	return f
 
 def heal_target(amount=0):
 	def x(target):
